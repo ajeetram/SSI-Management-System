e601d03dc88a231e1578fd63a7cac4d8001b2655
Ajeet Ram Verma <86039147+ajeetram@users.noreply.github.com>
2023-04-15 18:29:22 +0530

    updated

--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -9,15 +9,16 @@ const Home = () => {
     <img src={hero} alt="hero"></img>
     </div>
     <div className='col-2'>
-      <h1>Consent-Based Data</h1>
+      <h1>Self-Sovereign Identity</h1>
       <h1>Management System</h1>
       <div className='about'>
-        <p>A consent-based application using a public blockchain provides users with more control over their data and ensures transparency and security in all transactions and data access.</p>
+        <p>Self-sovereign identity (SSI) is a concept that allows individuals to control their own personal data and identity without relying on a centralized authority or third-party.</p>
       </div>
       <div className='btn'>
       <Link to="/issuer" className="button">Issue A Credential</Link>
       </div>
     </div>
+
     </div>
   )
 }
